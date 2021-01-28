2181b83f3e0060c525d92d959bdeca4b7fb0e81e
Tech Archiact <jlecoui@msn.com>
2021-01-27 20:51:27 -0800

    key range fix

--- hotkey/user_commands.py
+++ hotkey/user_commands.py
@@ -7,7 +7,7 @@ def frame_selected_timeline():
     cmds.playbackOptions(min=rng[0], max=rng[1])
 
 
-def key_range(sel=cmds.ls(selection=True)):
+def key_range(sel):
     time_range = scene_util.get_timeline_selection()
     for i in range(int(time_range[0]), int(time_range[1])):
         cmds.currentTime(i, update=True)
