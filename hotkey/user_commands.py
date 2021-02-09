718792df8f7ac78fac073a4f3a99f251c19ddb92
Tech Archiact <jlecoui@msn.com>
2021-02-09 11:41:55 -0800

    key frame range

--- hotkey/user_commands.py
+++ hotkey/user_commands.py
@@ -11,7 +11,7 @@ def key_range(sel):
     time_range = scene_util.get_timeline_selection()
     original_time = cmds.currentTime(query=True)
     for i in range(int(time_range[0]), int(time_range[1])):
-        cmds.currentTime(i, update=False)
+        cmds.currentTime(i, update=True)
         cmds.setKeyframe(sel)
     cmds.currentTime(original_time)
 
